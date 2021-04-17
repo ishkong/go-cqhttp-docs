6a393f63d69c0cf8d1f3c255d7808a43ef00205b
shkong <i@shkong.cc>
2021-04-17 13:31:10 +0800

    📈 Abandon the plugin and use js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,11 +13,24 @@ module.exports = {
                 message: "发现新的内容",
                 buttonText: "刷新",
             }
-        },
-        '@vuepress/google-analytics': {
-            'ga': 'UA-193666178-1'
         }
     },
+    head: [
+        [
+            'script',
+            {
+                async: true,
+                src: 'https://www.googletagmanager.com/gtag/js?id=G-CZJ8X185MG',
+            }
+        ],
+        [
+            'script',
+            {},
+            [
+                "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-CZJ8X185MG');",
+            ],
+        ],
+    ],
     themeConfig: {
         search: true,
         searchMaxSuggestions: 5,
